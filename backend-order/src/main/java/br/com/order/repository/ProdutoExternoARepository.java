9ab18de4d6a7afaa50f2c853f0b14c90ad45719e
sidinelson <sidicley.bartolomeu@gmail.com>
2025-02-09 23:44:39 -0300

    Add melhorias

--- backend-order/src/main/java/br/com/order/repository/ProdutoExternoARepository.java
+++ backend-order/src/main/java/br/com/order/repository/ProdutoExternoARepository.java
@@ -2,11 +2,9 @@ package br.com.order.repository;
 
 
 
-import br.com.order.model.ProdutoExternoAModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 @Repository
 public interface ProdutoExternoARepository extends JpaRepository<ProdutoExternoAModel, Long> {
 
